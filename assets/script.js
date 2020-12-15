d065bf8598372aad1182a688bd7f7d83f7dbd91b
mmmangooo <isabelle.angsas@gmail.com>
2020-12-15 10:46:15 +0000

    Added code for sending email through email.js on contact form, and success/fail-messages

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+/* Landing page */
+
 //jQuery code for fading out the 'landing page'-overlay in index.html, and showing the content hidden under it//
 
 $('#map-link').click(function() {
@@ -6,3 +8,34 @@ $('#map-link').click(function() {
     });
 })
 
+
+
+
+/* Contact form */
+
+// Making an email being sent through email.js on the submission of contact form
+
+function sendMail(contactForm) {
+emailjs.send('default_service', 'template_hhpfrte', {
+    'from_name': contactForm.name.value,
+    'form_email': contactForm.emailaddress.value,
+    'message': contactForm.message.value
+})
+
+// Creates a response for the user to see if the email has been successfully sent, or if the sending has failed
+
+.then(
+
+    function(response){
+        console.log('success', response);
+        document.getElementById('success-alert').style.display='block';
+    },
+
+    function(error){
+       console.log('failed', error);
+       document.getElementById('error-alert').style.display='block';
+    });
+
+return false;
+    
+}
